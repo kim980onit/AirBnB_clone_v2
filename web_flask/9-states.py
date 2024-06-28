0f168d1e50da6f260cf41ecbb67ab314296172cd
kimani kamau <kimanikamau971@gmail.com>
2024-06-28 12:57:57 +0300

    AirBnB_clone_v2

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
-"""flask app
 """
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from os import environ as env
 app = Flask(__name__)
 
 
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
 @app.teardown_appcontext
-def shutdown_session(exception=None):
-    """reload storage after request
-    """
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route("/states/<id>", strict_slashes=False)
-@app.route("/states", strict_slashes=False)
-def states_cities_list(id=None):
-    """show state and cities if id is available
-    otherwise list all states
-    """
-    states = storage.all("State")
-    if id:
-        state = states.get('State.{}'.format(id))
-        states = [state] if state else []
-    else:
-        states = list(states.values())
-    states.sort(key=lambda x: x.name)
-    for state in states:
-        state.cities.sort(key=lambda x: x.name)
-    return render_template(
-        '9-states.html',
-        states=states,
-        len=len(states),
-        id=id
-    )
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
